314daccf9ac5a09a6f45a738ee0def69ec353805
sethusaim <sethusaim@gmail.com>
2022-03-19 20:18:48 +0530

    made new changes

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ from phising.model.prediction_from_model import Prediction
 from phising.model.training_model import Train_Model
 from phising.validation_insertion.prediction_validation_insertion import Pred_Validation
 from phising.validation_insertion.train_validation_insertion import Train_Validation
-from utils.log_tables import Create_Log_Table
 from utils.read_params import read_params
 
 os.putenv("LANG", "en_US.UTF-8")
@@ -48,10 +47,6 @@ async def trainRouteClient():
     try:
         raw_data_train_bucket_name = config["s3_bucket"]["phising_raw_data"]
 
-        table = Create_Log_Table()
-
-        table.generate_log_tables(type="train")
-
         time.sleep(5)
 
         train_val = Train_Validation(bucket_name=raw_data_train_bucket_name)
@@ -76,10 +71,6 @@ async def trainRouteClient():
 async def predictRouteClient():
     try:
         raw_data_pred_bucket_name = config["s3_bucket"]["phising_raw_data"]
-
-        table = Create_Log_Table()
-
-        table.generate_log_tables(type="pred")
 
         pred_val = Pred_Validation(raw_data_pred_bucket_name)
 
--- utils/logger.py
+++ utils/logger.py
@@ -1,103 +1,60 @@
+import os
 from datetime import datetime
-import boto3
 
 
 class App_Logger:
-    """
-    Description :   This class is used for logging the info to DynamoDB
-    Written by  :   iNeuron Intelligence
-    
-    Version     :   1.2
-    Revisions   :   Moved to setup to cloud 
-    """
-
     def __init__(self):
-        self.db_resource = boto3.resource("dynamodb")
-
         self.class_name = self.__class__.__name__
 
-    def log(self, table_name, log_info):
-        """
-        Method Name :   log
-        Description :   This method is used for logging the info to DynamoDB
+        os.makedirs("logs", exist_ok=True)
 
-        Output      :   A log is created is DynamoDB table
-        On Failure  :   Write an exception log and then raise an exception
-
-        Version     :   1.2
-        Revisions   :   moved setup to cloud
-        """
-        method_name = self.log.__name__
-
+    def log(self, log_file, log_info):
         try:
-            self.table = self.db_resource.Table(table_name)
-
             self.now = datetime.now()
 
-            self.date = self.now.date()
+            self.date = self.now.strftime("%d:%m:%Y")
 
             self.current_time = self.now.strftime("%H:%M:%S")
 
-            log = {
-                "Log_updated_date": str(self.now),
-                "Log_updated_time": str(self.current_time),
-                "Log_message": log_info,
-            }
+            log_file_path = "logs" + "/" + log_file
 
-            self.table.put_item(Item=log)
+            with open(file=log_file_path, mode="a+") as f:
+                f.write(
+                    str(self.date)
+                    + "\t"
+                    + str(self.current_time)
+                    + "\t"
+                    + log_info
+                    + "\n"
+                )
+
+                f.close()
 
         except Exception as e:
-            error_msg = f"Exception occured in Class : {self.class_name}, Method : {method_name}, Error : {str(e)}"
+            raise e
 
-            raise Exception(error_msg)
-
-    def start_log(self, key, class_name, method_name, table_name):
-        """
-        Method Name :   start_log
-        Description :   This method creates an entry point log in DynamoDB
-
-        Output      :   An entry point is created in DynamoDB
-        On Failure  :   Write an exception log and then raise an exception
-
-        Version     :   1.2
-        Revisions   :   moved setup to cloud
-        """
-
+    def start_log(self, key, log_file, class_name, method_name):
         start_method_name = self.start_log.__name__
 
         try:
             func = lambda: "Entered" if key == "start" else "Exited"
 
-            log_msg = f"{func()} {method_name} method of class {class_name}"
+            key = func()
 
-            self.log(table_name, log_info=log_msg)
+            log_msg = f"{key} {method_name} method of class {class_name}"
+
+            self.log(log_file, log_msg)
 
         except Exception as e:
             error_msg = f"Exception occured in Class : {self.class_name}, Method : {start_method_name}, Error : {str(e)}"
 
             raise Exception(error_msg)
 
-    def exception_log(self, error, class_name, method_name, table_name):
-        """
-        Method Name :   exception_log
-        Description :   This method creates an exception log in DynamoDB and raises Exception
-
-        Output      :   A exception log is created in DynamoDB and expection is raised
-        On Failure  :   Write an exception log and then raise an exception
-
-        Version     :   1.2
-        Revisions   :   moved setup to cloud
-        """
-
-        self.start_log(
-            key="exit",
-            class_name=class_name,
-            method_name=method_name,
-            table_name=table_name,
-        )
+    def exception_log(self, error, log_file, class_name, method_name):
+        self.start_log("exit", log_file, class_name, method_name)
 
         exception_msg = f"Exception occured in Class : {class_name}, Method : {method_name}, Error : {str(error)}"
 
-        self.log(table_name=table_name, log_info=exception_msg)
+        self.log(log_file, exception_msg)
 
         raise Exception(exception_msg)
